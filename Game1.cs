b64862f35827f4762fa7bea0fa6c58b97bcd7015
Nicolas <nicdelgado03@Gmail.com>
2025-12-02 16:55:23 -0600

    Major UI implementations // saving to fix critical error in cntnt pipeline

--- Game1.cs
+++ Game1.cs
@@ -11,6 +11,10 @@ namespace ToeJam_Earl
         private readonly GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
         private CameraSystem _camera;
+        private MainMenu _mainMenu;
+        private CharacterMenu _characterMenu;
+        private bool _inMainMenu = true;
+        private bool _inCharacterMenu = false;
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
@@ -36,6 +40,8 @@ namespace ToeJam_Earl
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
+            _mainMenu = new MainMenu();
+            _characterMenu = new CharacterMenu();
             Globals.SpriteBatch = _spriteBatch;
             Texture2D tileset = Content.Load<Texture2D>("Island");
             Tiles tiles = new Tiles(tileset);
@@ -51,17 +57,37 @@ namespace ToeJam_Earl
             Globals.Update(gameTime);
             InputManager.Update();
 
-            // Land of 'If' 
-            //if (GameState.InMainMenu)
-            //{
-            //    // UpdateMainMenu();
-            //    return;
-            //}
-            //if (GameState.QuitRequest)
-            //{
-            //    Exit();
-            //    return;
-            //}
+            // Main Menu
+            if (_inMainMenu)
+            {
+                string? result = _mainMenu.Update();
+
+                if (result == "Play")
+                {
+                    _inMainMenu = false;
+                    _inCharacterMenu = true;
+                    return;
+                }
+                else if (result == "Quit")
+                {
+                    Exit();
+                    return;
+                }
+                return;
+            }
+            if (_inCharacterMenu)
+            {
+                string? character = _characterMenu.Update();
+
+                if (character == "ToeJam" || character == "Earl")
+                {
+                    GameManager.StartGame(character);
+                    _inCharacterMenu = false;
+                    return;
+                }
+                return;
+            }
+            // Normal Gameplay
             if (InputManager.PausePressed)
             {
                 GameState.TogglePause();
@@ -115,18 +141,25 @@ namespace ToeJam_Earl
 
         protected override void Draw(GameTime gameTime)
         {
-            //if (GameState.InMainMenu)
-            //{
-            //    DrawMainMenu();
-            //    return;
-            //}
             GraphicsDevice.Clear(Color.CornflowerBlue);
-
             _spriteBatch.Begin(transformMatrix: _camera.GetViewMatrix(), samplerState: SamplerState.PointClamp);
+            if (_inMainMenu)
+            {
+                _mainMenu.Draw(_spriteBatch);
+                _spriteBatch.End();
+                return;
+            }
+
+            if (_inCharacterMenu)
+            {
+                _characterMenu.Draw(_spriteBatch);
+                _spriteBatch.End();
+                return;
+            }
+
             _tileManager.Draw(_spriteBatch);
             _gameManager.Draw();
             _spriteBatch.End();
-
             base.Draw(gameTime);
 
         }
---
+++ Other/CharacterMenu.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+public class CharacterMenu
+{
+    private int _selectedIndex = 0;
+    private KeyboardState _prevKeyboard;
+    private SpriteFont _font;
+    private Texture2D _mainMenu;
+    private Rectangle _finger;
+    private Rectangle _bg;
+    private string _TJtxt = "one player -- jus' toejam";
+    private string _Etxt = "one player -- jus' earl";
+    private Vector2 _TJPos = new Vector2(400, 300);
+    private Vector2 _EPos = new Vector2(400, 360);
+    public CharacterMenu()
+    {
+        _font = Globals.Content.Load<SpriteFont>("TJEFont");
+        _mainMenu = Globals.Content.Load<Texture2D>("MainMenu");
+        _finger = new Rectangle(336, 24, 32, 15);
+        _bg = new Rectangle(8, 8, 320, 224);
+    }
+
+    public string? Update()
+    {
+        KeyboardState keyboard = Keyboard.GetState();
+
+        if (keyboard.IsKeyDown(Keys.Down) && _prevKeyboard.IsKeyUp(Keys.Down))
+        {
+            _selectedIndex++;
+            if (_selectedIndex > 1)
+                _selectedIndex = 0;
+        }
+        if (keyboard.IsKeyDown(Keys.Up) && _prevKeyboard.IsKeyUp(Keys.Up))
+        {
+            _selectedIndex--;
+            if (_selectedIndex < 0)
+                _selectedIndex = 1;
+        }
+        if (keyboard.IsKeyDown(Keys.Enter) && _prevKeyboard.IsKeyUp(Keys.Enter))
+        {
+            if (_selectedIndex == 0)
+                return "ToeJam";
+            else
+                return "Earl";
+        }
+        _prevKeyboard = keyboard;
+        return null; 
+    }
+
+    public void Draw(SpriteBatch spriteBatch)
+    {
+        spriteBatch.Draw(_mainMenu, destinationRectangle: new Rectangle(0, 0, 1024, 768), sourceRectangle: _bg, color: Color.White);
+        spriteBatch.DrawString(_font, _TJtxt, _TJPos, Color.White);
+        spriteBatch.DrawString(_font, _Etxt, _EPos, Color.White);
+        Vector2 fingerPos = ((_selectedIndex == 0) ? _TJPos + new Vector2(-60, -10) : _EPos + new Vector2(-60, -10));
+        spriteBatch.Draw(_mainMenu, position: fingerPos, sourceRectangle: _finger, color: Color.White);
+    }
+}
